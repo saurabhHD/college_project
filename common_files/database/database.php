d5bc742b933197b3a45b81201a5b9d4b57fb5f24
saurabhHD <saurabhinpurani@gmail.com>
2022-06-09 18:37:59 +0530

    add remote dbms

--- common_files/database/database.php
+++ common_files/database/database.php
@@ -3,7 +3,7 @@
 class database{
 	private $db;
 	function db(){
-		$this->db = new mysqli("localhost","root","","bookstore");
+		$this->db = new mysqli("156.67.222.85","u149648176_test","Test@123","u149648176_test");
 
 		if(!$this->db->connect_error)
 		{
