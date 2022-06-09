b746e7b8952af8b841ab786e78c0fd8bda5d9495
Saurabh Kumar <54767919+saurabhHD@users.noreply.github.com>
2022-06-09 10:03:12 +0530

    add remote rdbms

--- common_files/database/database.php
+++ common_files/database/database.php
@@ -3,7 +3,7 @@
 class database{
 	private $db;
 	function db(){
-		$this->db = new mysqli("localhost","root","","bookstore");
+		$this->db = new mysqli("sql.freedb.tech","freedb_saurabh","!8tC?MG@sNPm6xG","freedb_college_project");
 
 		if(!$this->db->connect_error)
 		{
