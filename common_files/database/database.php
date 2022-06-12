e191362038a78b731735e8366b402ce86587c214
saurabhHD <saurabhinpurani@gmail.com>
2022-06-12 09:00:05 +0530

    fix database connection with digital ocean

--- common_files/database/database.php
+++ common_files/database/database.php
@@ -3,7 +3,7 @@
 class database{
 	private $db;
 	function db(){
-		$this->db = new mysqli("sql149.main-hosting.eu","u149648176_test","Test@123","u149648176_test");
+		$this->db = new mysqli("college-project-do-user-11603524-0.b.db.ondigitalocean.com","25060","doadmin","AVNS_5HpL1Y6y4kgUgKdjbtW","defaultdb");
 
 		if(!$this->db->connect_error)
 		{
